294ce673670d0afbfd69b6c46ac99d07390e7d23
Star-Lord7 <samuelnasa375@gmail.com>
2025-09-21 19:00:05 -0600

    Opciones en la Conexion a Base de Datos

--- config/db.php
+++ config/db.php
@@ -1,34 +1,34 @@
 <?php
 
+// require_once __DIR__ . '/../vendor/autoload.php';
+
+// use Dotenv\Dotenv; //libreria para cargar variables de entorno
+
+// $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+// $dotenv->load();
+
 // function conectarDB() : mysqli {
-//     $db = mysqli_connect('localhost', 'root', 'root', 'crud_kodigo');
+//     $host = $_ENV['MYSQL_ADDON_HOST']; // MYSQL_ADDON_HOST
+//     $user = $_ENV['MYSQL_ADDON_USER']; // MYSQL_ADDON_USER
+//     $pass = $_ENV['MYSQL_ADDON_PASSWORD']; // MYSQL_ADDON_PASSWORD
+//     $db   = $_ENV['MYSQL_ADDON_DB']; // MYSQL_ADDON_DB
+//     $port = $_ENV['MYSQL_ADDON_PORT']; // MYSQL_ADDON_PORT
+
+//     $db = mysqli_connect($host, $user, $pass, $db, $port);
 
 //     if(!$db){
-//         echo 'Error no se pudo conectar a la base de datos';
+//         echo 'Error: No se pudo conectar a la base de datos -> ' . mysqli_connect_error();
 //         exit;
 //     }
 //     return $db;
 // }
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use Dotenv\Dotenv; //libreria para cargar variables de entorno
-
-$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
-
 function conectarDB() : mysqli {
-    $host = $_ENV['MYSQL_ADDON_HOST']; // MYSQL_ADDON_HOST
-    $user = $_ENV['MYSQL_ADDON_USER']; // MYSQL_ADDON_USER
-    $pass = $_ENV['MYSQL_ADDON_PASSWORD']; // MYSQL_ADDON_PASSWORD
-    $db   = $_ENV['MYSQL_ADDON_DB']; // MYSQL_ADDON_DB
-    $port = $_ENV['MYSQL_ADDON_PORT']; // MYSQL_ADDON_PORT
+    $db = mysqli_connect('localhost', 'root', 'root', 'crud_kodigo');
 
-    $conexion = mysqli_connect($host, $user, $pass, $db, $port);
-
-    if(!$conexion){
-        echo 'Error: No se pudo conectar a la base de datos -> ' . mysqli_connect_error();
+    if(!$db){
+        echo 'Error no se pudo conectar a la base de datos';
         exit;
     }
-    return $conexion;
+    return $db;
 }
